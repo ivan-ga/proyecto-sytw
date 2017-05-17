e37375395ab652d929dd8b4cb9abfb089bf47d5c
Miguel Parra <alu0100200393@ull.edu.es>
2017-05-17 10:58:47 +0000

    cambios finales

--- config/auth.js
+++ config/auth.js
@@ -3,16 +3,16 @@ module.exports = {
   facebookAuth: {
     clientID: '1416333221758942',
     clientSecret: 'f5f30eca089742b5757af51489cea257',
-    callbackURL: '/auth/facebook/callback',
+    callbackURL: 'https://dsi-proyecto-miguelpe83.c9users.io/auth/facebook/callback',
   },
   twitterAuth: {
     consumerKey: 'llaoII8ao14jSMHrhHCHCVWeC',
     consumerSecret: 'hEYxgG3oxjDR0R1t9EWZ98tBKZ1IlVacHAQn5LDuvCIgzJbc9q',
-    callbackURL: 'https://localhost:8080/auth/twitter/callback',
+    callbackURL: 'https://dsi-proyecto-miguelpe83.c9users.io/auth/twitter/callback',
   },
   githubAuth: {
     clientID: '4da3fc9817dc296aee71',
     clientSecret: '0ef3f4b8fe00806df1ef8f734e2a47332918830f',
-    callbackURL: 'https://localhost:8080/login/github/return',
+    callbackURL: 'https://dsi-proyecto-miguelpe83.c9users.io/login/github/return',
   },
 };
