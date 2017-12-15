1d0991232851610750f034ac0c213891c429222f
alu0100851236 <alu0100851236@ull.edu.es>
2017-12-15 14:42:04 +0000

    arreglos varios

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-//var upload = require('./routes/upload');
-
-
 
 var app = express();
 
@@ -44,105 +41,6 @@ app.use('/users', users);
 
 const conexionBD = require('./config/database.js');
 mongoose.connect(conexionBD.url);
-
-//Importando esquemas de BD
-const Estructura = require('./models/user.js');
-const userSchema = Estructura.User;
-
-const User = mongoose.model("User", userSchema);
-
-app.get('/m', (request, response) => {
-
-    User.find({}, function(err,data)
-    {
-        if(err)  console.error("Error:"+err);
-        else
-        {
-              if( request.query.ganadas_3enraya != undefined){
-
-                    for (var i = data.length - 1; i >= 0; i--) {
-                          if( data[i].id === request.session.passport.user ){
-                          User.update({"_id": data[i]._id}, {$inc: {"local.ganadas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
-                          });
-                    }
-              }
-
-              }
-              if( request.query.perdidas_3enraya  != undefined){
-
-                    for (var i = data.length - 1; i >= 0; i--) {
-                        if(data[i].id === request.session.passport.user){
-                          User.update({"_id": data[i]._id}, {$inc: {"local.perdidas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
-
-                          });
-
-                        }
-                     }
-
-              }
-              if( request.query.empatadas_3enraya  != undefined){
-
-                    for (var i = data.length - 1; i >= 0; i--) {
-                        if(data[i].id === request.session.passport.user){
-
-                          User.update({"_id": data[i]._id}, {$inc: {"local.empatadas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
-
-                          });
-
-                        }
-                     }
-
-              }
-        }
-    });
-
-});
-
-
-
-app.get('/ranking', (req, res) => {
-
- User.find().sort('-local.ganadas_3enraya').find( function(err,data)
-  {
-      if(err)  console.error("Error:"+err);
-
-        res.render("ranking",{users: data,title: "Otro"})
-    })
-
-
-});
-
-//////////////////////SUBIR IMAGEN PARA PERFIL DE USUARIO
-
-var storage = multer.diskStorage({
-	destination: function(req, file, callback) {
-		callback(null, './public/img/uploads') 
-	},
-	filename: function(req, file, callback) {
-		callback(null, req.session.passport.user + path.extname(file.originalname))
-		
-	}
-})
-
-app.post('/home', function(req, res) {
-	var upload = multer({
-		storage: storage,
-		fileFilter: function(req, file, callback) {
-			var ext = path.extname(file.originalname)
-			if (ext !== '.jpg') {
-				return callback(res.end('Only jpg images are allowed'), null)
-			}
-			callback(null, true)
-		}
-	}).single('userFile');
-	upload(req, res, function(err) {
-	  
-	})
-	
-	res.redirect('back'); //refresca la página después de cambiar la foto de perfil
-})
-
-//////////////////////TERMINADO SUBIR IMAGEN
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var path = require('path');
 var passport = require('passport');
+var mongoose = require('mongoose');
+var multer = require('multer');
 var router = express.Router();
+
+//Importando esquemas de BD
+const Estructura = require('../models/user.js');
+const userSchema = Estructura.User;
+
+const User = mongoose.model("User", userSchema);
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -50,6 +59,37 @@ router.get('/g_buscaminas', isLoggedIn, function(req, res) {
   res.render('g_buscaminas.ejs', { user: req.user,title: "Buscaminas" });
 });
 
+router.get('/r_tresenraya', isLoggedIn, function(req, res) {
+  User.find().sort('-local.ganadas_3enraya').find( function(err,data)
+   {
+       if(err)  console.error("Error:"+err);
+        res.render('r_tresenraya.ejs', { user: data,title: "3 en raya" })
+     })
+});
+
+router.get('/r_ajedrez', isLoggedIn, function(req, res) {
+  User.find().sort('-local.ganadas_ajedrez').find( function(err,data)
+   {
+       if(err)  console.error("Error:"+err);
+        res.render('r_ajedrez.ejs', { user: data,title: "Ajedrez" })
+     })
+});
+
+router.get('/r_buscaminas', isLoggedIn, function(req, res) {
+  User.find().sort('-local.ganadas_buscaminas').find( function(err,data)
+   {
+       if(err)  console.error("Error:"+err);
+        res.render('r_buscaminas.ejs', { user: data,title: "Buscamnias" })
+     })
+});
+
+router.get('/r_damas', isLoggedIn, function(req, res) {
+  User.find().sort('-local.ganadas_damas').find( function(err,data)
+   {
+       if(err)  console.error("Error:"+err);
+        res.render('r_damas.ejs', { user: data,title: "Damas" })
+     })
+});
 
 router.post('/registro', passport.authenticate('local-signup', {
   successRedirect: '/home',
@@ -84,6 +124,86 @@ router.get('/login/github/return', passport.authenticate('github', {
   failureRedirect: '/',
 }));
 
+// ### ACTUALIZAR ESTADÍSTICAS ###
+router.get('/m', isLoggedIn, (request, response) => {
+
+    User.find({}, function(err,data)
+    {
+        if(err)  console.error("Error:"+err);
+        else
+        {
+              if( request.query.ganadas_3enraya != undefined){
+
+                    for (var i = data.length - 1; i >= 0; i--) {
+                          if( data[i].id === request.session.passport.user ){
+                          User.update({"_id": data[i]._id}, {$inc: {"local.ganadas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
+                          });
+                    }
+              }
+
+              }
+              if( request.query.perdidas_3enraya  != undefined){
+
+                    for (var i = data.length - 1; i >= 0; i--) {
+                        if(data[i].id === request.session.passport.user){
+                          User.update({"_id": data[i]._id}, {$inc: {"local.perdidas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
+
+                          });
+
+                        }
+                     }
+
+              }
+              if( request.query.empatadas_3enraya  != undefined){
+
+                    for (var i = data.length - 1; i >= 0; i--) {
+                        if(data[i].id === request.session.passport.user){
+
+                          User.update({"_id": data[i]._id}, {$inc: {"local.empatadas_3enraya":1,"local.totales_3enraya":1}},function(error,dato){
+
+                          });
+
+                        }
+                     }
+
+              }
+        }
+    });
+
+});
+
+//////////////////////SUBIR IMAGEN PARA PERFIL DE USUARIO
+
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './public/img/uploads')
+	},
+	filename: function(req, file, callback) {
+		callback(null, req.session.passport.user + path.extname(file.originalname))
+
+	}
+})
+
+router.post('/home', isLoggedIn, function(req, res) {
+  console.log("POST UPLOAD");
+	var upload = multer({
+		storage: storage,
+		fileFilter: function(req, file, callback) {
+			var ext = path.extname(file.originalname)
+			if (ext !== '.jpg') {
+				return callback(res.end('Only jpg images are allowed'), null)
+			}
+			callback(null, true)
+		}
+	}).single('userFile');
+	upload(req, res, function(err) {
+
+	})
+
+	res.redirect('back'); //refresca la página después de cambiar la foto de perfil
+})
+
+//////////////////////TERMINADO SUBIR IMAGEN
 
 module.exports = router;
 
